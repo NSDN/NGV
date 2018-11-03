3a88db52ce2c92c0ce2c72974749becc183a9b66
drzzm32 <drzzm32@gmail.com>
2018-11-03 21:14:50 +0800

    modify makefile

--- cortex/emulator/make-win.bat
+++ cortex/emulator/make-win.bat
@@ -5,7 +5,9 @@ if exist .\Output\ (
 )
 mkdir .\Output\
 
-echo Compiling binary...
+echo Compiling console binary...
+g++ -std=c++11 -static-libstdc++ -static-libgcc -O3 -B ./ -DNGV_SYS_VERSION="""%date:~2,2%%date:~5,2%%date:~8,2%""" ./Drivers/NGV/*.c ./Drivers/NSASM/*.cpp ./Drivers/NSGDX/*.cpp *.cpp -LDrivers/SDL2/Lib -lm -lmingw32 -lSDL2main -lSDL2 -o ./Output/ngv-emu
+echo Compiling windows binary...
 g++ -std=c++11 -mwindows -static-libstdc++ -static-libgcc -O3 -B ./ -DNGV_SYS_VERSION="""%date:~2,2%%date:~5,2%%date:~8,2%""" ./Drivers/NGV/*.c ./Drivers/NSASM/*.cpp ./Drivers/NSGDX/*.cpp *.cpp -LDrivers/SDL2/Lib -lm -lmingw32 -lSDL2main -lSDL2 -o ./Output/ngv-emu-win
 echo Done
 
